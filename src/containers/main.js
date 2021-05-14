dc5aa5be82fa5465f466802f4a308c97fb0d489a
salavat876 <mavlikhanov2002@mail.ru>
2021-05-14 18:44:47 +0400

    update mainComponent

--- src/containers/main.js
+++ src/containers/main.js
@@ -34,7 +34,24 @@ function Main () {
                 </Grid>
 
                 <Grid item xs={6}>
-                    <div className="bg-black"></div>
+                    <div>
+                        <div className="header-img-block">
+                            <Avatar className={classes.purpleAvatar}>N</Avatar>
+                            <Box className={classNames(classes.colorBlack,classes.nickName,classes.nickNameMain)}  component="span">
+                                NickName
+                            </Box>
+                        </div>
+                        <div className="main-img">
+                            <img src={testImg}/>
+                        </div>
+
+                        <div className="main-img_like">
+                            <section className="like-button-group">
+                                <button className="like-btn btn"></button>
+                                <button className="magnifier btn"></button>
+                            </section>
+                        </div>
+                    </div>
                 </Grid>
 
             </Grid>
