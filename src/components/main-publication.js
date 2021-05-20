d7e41c64fcc77e6907b23fa22435f92bbb276aa6
salavat876 <mavlikhanov2002@mail.ru>
2021-05-20 23:51:40 +0400

    add new component

---
+++ src/components/main-publication.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Avatar ,Grid,Box } from '@material-ui/core';
+import classNames from 'classnames';
+import {makeStyles} from '@material-ui/core/styles';
+import LikeBtnGroup from '../components/likeGroup';
+
+const MainPublication = ({imgSrc}) =>{
+
+    const classes = useStyles();
+
+    return(
+        <Grid item xs={6}>
+                    <div>
+                        <div className="header-img-block">
+                            <Avatar className={classes.purpleAvatar}>N</Avatar>
+                            <Box className={classNames(classes.colorBlack,classes.nickName,classes.nickNameMain)}  component="span">
+                                NickName
+                            </Box>
+                        </div>
+                        <div className="main-img">
+                            <img src={imgSrc[0]}/> 
+                        </div>
+
+                        <div className="main-img_like">
+                            <LikeBtnGroup />
+                        </div>
+                    </div>
+                </Grid>
+    )
+}
+
+
+const useStyles = makeStyles({
+    purpleAvatar:{
+        backgroundColor:'purple'
+    },
+    colorBlack:{
+        color:'#000000'
+    },
+
+    nickName:{
+        marginRight:'10px'
+    },
+    nickNameMain:{
+        margin:'0 0 0 20px',
+    }
+})
+
+export default MainPublication;
