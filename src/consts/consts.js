a033ed2498da42122121e40131d24e9ccc925d1d
salavat876 <mavlikhanov2002@mail.ru>
2021-07-29 00:22:40 +0400

    build app

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -3,4 +3,4 @@ export const redirect_url = 'urn:ietf:wg:oauth:2.0:oob',
     secret = 'vlJy_e3ElNhZFQntKWkm653HgKv0JpU1dj1Ln7NOB64',
     grant_type = 'authorization_code',
     url = 'https://unsplash.com/oauth/token',
-    code = `icGNIL2O9pTu9WbrBjhn3d50DUqYYzt3fOx9_R5pzx4 `;
+    code = `3rczi7zQn4KUNEV4AK9c1Pc3IKerwje58N4we6A-p_E `;
