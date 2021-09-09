83bc46c0feb6a929914d1ccdf20bfe688db58803
salavat876 <mavlikhanov2002@mail.ru>
2021-09-09 17:54:46 +0400

    почти сделал слайдер

--- src/components/main-publication/like-group/slider/Slider.js
+++ src/components/main-publication/like-group/slider/Slider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css'
 import {useDispatch, useSelector} from "react-redux";
 
@@ -8,23 +8,47 @@ const Slider = () => {
     const dispatch = useDispatch();
     const {isOpened,currentId}= useSelector(({openSlider})=> openSlider)
     const arrayPhotos = useSelector(({initialLikes})=> initialLikes.arrPhotos);
+    const [current,setCurrent] = useState(arrayPhotos.findIndex(item => item.id === currentId));
+    const length = arrayPhotos.length
 
      const currentPhoto = arrayPhotos.filter((photo)=> {
         return photo.id === currentId
      })
+
+    const nextPhoto = () => {
+        setCurrent(current === length - 1 ? 0: current + 1)
+        console.log(current)
+    }
+
+    const prevPhoto = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+        console.log(current)
+    }
     const handleCloseSlider = () => {
       dispatch(hideSlider())
     }
     return (
         <div className = {isOpened ? "modal-overlay active":"modal-overlay"} onClick={handleCloseSlider}>
-            
                 <div className="modal-content" onClick={e => e.stopPropagation()} >
-                    <img src={currentPhoto.map(item => item.urls.raw)}/>
+                    <img src={currentPhoto.map((item,index) => {
+                        if(index === current) {
+                            return item.urls.raw
+                        }
+                    })}/>
                 </div>
 
             <div className="close" onClick={handleCloseSlider}></div>
-            <div className="slider-btn btn-left"  onClick={e => e.stopPropagation()}></div>
-            <div className ="slider-btn btn-right" onClick={e => e.stopPropagation()}></div>
+
+            <div className="slider-btn btn-left"
+                 onClick={e => {
+                prevPhoto()
+                e.stopPropagation()
+            }}></div>
+            <div className ="slider-btn btn-right" onClick={e => {
+                nextPhoto()
+                e.stopPropagation()
+            }}></div>
+
         </div>
     )
 }
