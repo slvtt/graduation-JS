adc9cd6ea397335c9e4359b19aa08ab314024208
salavat876 <mavlikhanov2002@mail.ru>
2021-09-28 18:09:14 +0400

    refresh code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
-
-import {defaultReducer} from './redux/reducers/authReducer'
-import {loadState,saveState} from './redux/localStorage/localStorage';
+import {Route} from "react-router";
 import {BrowserRouter} from "react-router-dom";
-
-import App from './containers/app';
 
 import 'normalize.css';
 import './sass/index.scss';
-import  cors from 'cors';
-import {Route} from "react-router";
+
+import App from './containers/app';
 import reducer from './redux/reducers/indexReducer'
+import {loadState,saveState} from './redux/localStorage/localStorage';
 
 const state = loadState()
 const store = createStore(reducer,state,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
